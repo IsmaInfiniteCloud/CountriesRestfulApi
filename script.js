abb0118c236c1f64220e87e23b45d73a17dc59d2
manou141 <manou141@hotmail.com>
2023-04-26 11:32:45 -0400

    changed the background color

--- script.js
+++ script.js
@@ -23,7 +23,7 @@
 //  })
 //  .catch((error) => console.log(error));
 
-  const container = document.querySelector('.container');
+const container = document.querySelector('.container');
 
 fetch('https://restcountries.com/v3.1/all')
   .then((response) => response.json())
@@ -63,13 +63,23 @@ fetch('https://restcountries.com/v3.1/all')
     data.forEach((country) => {
       const card = document.createElement('div');
       card.classList.add('card');
+
       const name = document.createElement('h2');
       name.textContent = country.name.common;
+
       const population = document.createElement('p');
-      population.textContent = `Population: ${country.population}`;
+      population.textContent = `السكان: ${country.population}`;
       const coatOfArms = document.createElement('img');
+
       const area = document.createElement('h1');
       area.textContent = `المساحة: ${country.area}`;
+      
+      const maps = document.createElement('h4');
+      const mapLink = document.createElement('a');
+      mapLink.href = country.maps.googleMaps;
+      mapLink.textContent = 'Map';
+      maps.textContent = `Map : `;
+
       coatOfArms.src = country.coatOfArms.png;
       coatOfArms.alt = `${country.name.common} coat of arms`;
       const flag = document.createElement('img');
@@ -80,6 +90,14 @@ fetch('https://restcountries.com/v3.1/all')
       card.appendChild(flag);
       card.appendChild(population);
       card.appendChild(area);
+      if (country.region) { // Check if region exists before creating the element
+        const region = document.createElement('h3');
+        region.textContent =` Region :${country.region}`;
+        card.appendChild(region);
+      }
+      maps.appendChild(mapLink);
+      card.appendChild(maps); 
+
       container.appendChild(card);
     });
   })
